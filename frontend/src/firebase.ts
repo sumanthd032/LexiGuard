63dccb3509108af35aff526ce348a0b26f968ea4
sumanthd032 <sumanthd032@gmail.com>
2025-09-16 23:40:21 +0530

    fix(secret): remove unecessary files

--- frontend/src/firebase.ts
+++ frontend/src/firebase.ts
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCvVbteq4ZeKuugreO_g4eAcW--XIXUimk",
-  authDomain: "lexiguard-fb92e.firebaseapp.com",
-  projectId: "lexiguard-fb92e",
-  storageBucket: "lexiguard-fb92e.firebasestorage.app",
-  messagingSenderId: "108801331237",
-  appId: "1:108801331237:web:e21af53f20c203f8b6afcc"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
